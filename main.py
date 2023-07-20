a21a36020eb595c3ca3e880e5b039e346578d9d5
Fathidev <fathi.devops@gmail.com>
2023-07-20 09:13:10 +0200

    functions

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
 import qrcode
 import datetime
 from PIL import Image
+
+# functions to get the date, the extension and the full name of the file
+def getDate ():
+    dateToday = datetime.date.today()
+    return dateToday
+
+def makeExtension(choice):
+    if choice == 1:
+        return ".png"
+    elif choice == 2:
+        return ".jpg"
+    elif choice == 3:
+        return ".jpeg"
+    else:
+        return ".png"
+    
+def makeFullName (name, extension, dateToday):
+    return f'{dateToday}_{name}{extension}'
+# end of functions
